501fb4609e797930eafbf122a0c6a92b347b8728
theoigen <oigenZ@icloud.com>
2019-06-26 22:58:29 +0300

    changed server port to 3000, putted semi everywhere

--- server/app.js
+++ server/app.js
@@ -1,20 +1,16 @@
-const path = require('path')
-const history = require('connect-history-api-fallback')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const api = require('./routes/api')
+const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const api = require('./routes/api');
 
-const PORT = 8080
+const PORT = 3000;
 
-const publicPath = path.resolve(__dirname, '../../dist')
-const staticConf = { maxAge: '1y', etag: false }
+const publicPath = path.resolve(__dirname, '../../dist');
 
-app.use(bodyParser.json())
-app.use('/api', api)
+app.use(bodyParser.json());
+app.use('/api', api);
 
-app.use(express.static(publicPath, staticConf))
+app.use(express.static(publicPath));
 
-app.use('/', history())
-
-app.listen(PORT, () => console.log(`App running on port ${PORT}!`))
+app.listen(PORT, () => console.log(`App running on port ${PORT}!`));
