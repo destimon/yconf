a0539d843a34db05a113dbfb305ec01f5a772ab2
dimas <destaom@gmail.com>
2019-06-27 23:15:24 +0300

    test.vue + main.scss test

---
+++ client/src/views/Test.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <ul class="yc-ul">
+            <li class="yc-li">one</li>
+            <li class="yc-li">two</li>
+            <li class="yc-li">three</li>
+            <li class="yc-li">four</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+    @import "@/assets/main.scss";
+
+</style>
